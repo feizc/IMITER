c4105213f5747af23479a8b0914532586aa399d3
费政聪 <3300949577@qq.com>
2022-06-05 15:36:06 +0800

    add imitation mixed attention

--- model/IMITER.py
+++ model/IMITER.py
@@ -1,6 +1,7 @@
 import torch 
-import torch.nn as nn 
 import math 
+import torch.nn as nn 
+import torch.nn.functional as F
 
 
 
@@ -16,6 +17,10 @@ class IMITERSelfAttention(nn.Module):
                 f"heads {config.num_attention_heads}."
             )
 
+        self.text_seq_len = config.max_position_embeddings 
+        num_patch = int(config.image_size / config.patch_size) 
+        self.image_seq_len = num_patch * num_patch + 1  # One token for global fusion 
+
         self.num_attention_heads = config.num_attention_heads
         self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
         self.all_head_size = self.num_attention_heads * self.attention_head_size 
@@ -24,23 +29,69 @@ class IMITERSelfAttention(nn.Module):
         self.key = nn.Linear(config.hidden_size, self.all_head_size, bias=config.qkv_bias)
         self.value = nn.Linear(config.hidden_size, self.all_head_size, bias=config.qkv_bias)
 
-        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob) 
+
+        # Imitation for image and text fusion 
+        self.imitate_text_key = nn.Linear(self.image_seq_len, self.text_seq_len) 
+        self.imitate_text_value = nn.Linear(self.image_seq_len, self.text_seq_len) 
+        self.imitate_image_key = nn.Linear(self.text_seq_len, self.image_seq_len) 
+        self.imitate_image_value = nn.Linear(self.text_seq_len, self.image_seq_len)
+
 
     def transpose_for_scores(self, x):
         new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
         x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
+        return x.permute(0, 2, 1, 3) 
 
-    def forward(self, hidden_states, attention_mask=None, head_mask=None, output_attentions=False):
+
+    def imitate_key_matrix(self, key_layer): 
+        t_key_layer = key_layer.permute(0, 1, 3, 2) # (bsz, num_head, head_size, L_text+L_image)
+        text_m, image_m = torch.split(t_key_layer, (self.text_seq_len, self.image_seq_len), dim=-1) 
+        predict_image_m = self.imitate_image_key(text_m) 
+        predict_text_m = self.imitate_text_key(image_m)
+        return torch.cat((predict_text_m, predict_image_m), dim=-1).permute(0, 1, 3, 2) 
+
+
+    def imitate_value_matrix(self, value_layer): 
+        t_value_layer = value_layer.permute(0, 1, 3, 2) # (bsz, num_head, head_size, L_text+L_image)
+        text_m, image_m = torch.split(t_value_layer, (self.text_seq_len, self.image_seq_len), dim=-1) 
+        predict_image_m = self.imitate_image_value(text_m) 
+        predict_text_m = self.imitate_text_value(image_m)
+        return torch.cat((predict_text_m, predict_image_m), dim=-1).permute(0, 1, 3, 2)
+
+    def compute_imitation_loss(self, key_layer, value_layer, imitate_key_layer, imitate_value_layer): 
+        loss = 0 
+        loss += F.mse_loss(imitate_key_layer, key_layer) 
+        loss += F.mse_loss(imitate_value_layer, value_layer) 
+        return loss
+
+
+    def forward(
+            self, 
+            hidden_states, 
+            attention_mask=None, 
+            head_mask=None, 
+            output_attentions=False, 
+            predicted_usage=True, # Use the imitated key, value forward 
+        ):
+
         mixed_query_layer = self.query(hidden_states)
 
         key_layer = self.transpose_for_scores(self.key(hidden_states))
         value_layer = self.transpose_for_scores(self.value(hidden_states))
         query_layer = self.transpose_for_scores(mixed_query_layer) 
 
+        imitate_key_layer = self.imitate_key_matrix(key_layer) 
+        imitate_value_layer = self.imitate_value_matrix(value_layer) 
+
+        imitation_loss = self.compute_imitation_loss(key_layer, value_layer, imitate_key_layer, imitate_value_layer)
+
+        if predicted_usage == True: 
+            key_layer = imitate_key_layer 
+            value_layer = imitate_value_layer
+
         # Take the dot product between "query" and "key" to get the raw attention scores.
         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2)) 
-        print(attention_scores.size())
         attention_scores = attention_scores / math.sqrt(self.attention_head_size)
         if attention_mask is not None:
             # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
@@ -58,12 +109,12 @@ class IMITERSelfAttention(nn.Module):
             attention_probs = attention_probs * head_mask
 
         context_layer = torch.matmul(attention_probs, value_layer)
-        print(context_layer.size())
         context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
         context_layer = context_layer.view(*new_context_layer_shape)
 
-        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
+
+        outputs = (context_layer, imitation_loss, attention_probs) if output_attentions else (context_layer, imitation_loss,)
 
         return outputs  
 
