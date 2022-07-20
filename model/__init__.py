d8492acd52e87d1b24aa2ac66616642c09c6b512
费政聪 <3300949577@qq.com>
2022-07-20 09:36:45 +0800

    Add files via upload

--- model/__init__.py
+++ model/__init__.py
@@ -5,4 +5,9 @@ from .ITR_dual import *
 from .feature_extraction import * 
 from .loss import * 
 from .IMITER import * 
-from .vilt_cos import * 
+# clip loss for vilt 
+# from .vilt_cos import * 
+# vilt_split uniter model with split vector 
+# vilt_imiter employ weight_matric learning 
+# vilt_transfer employ meta embedding projection
+from .vilt_transfer import * 
